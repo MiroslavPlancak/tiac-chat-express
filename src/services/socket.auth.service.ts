1fee4476c74ca9713e2e5d5f763907aeb72de35b
Miroslav Plancak <mplancak@gmail.com>
2024-11-20 14:29:13 +0100

    implemented a mechanism for all cases of user disconnection from the server

--- src/services/socket.auth.service.ts
+++ src/services/socket.auth.service.ts
@@ -11,6 +11,7 @@ export class SocketAuthService {
     public authenticateUser(socketId: socketIO.Socket, userId: string) {
         this.clientConnectionSocketIdMap.set(userId, socketId.id)
         console.log(`Client authenticated: ${userId} with socket ID: ${socketId.id}`)
+        console.log(`map at login:`, this.clientConnectionSocketIdMap)
     }
 
     // Retrieve socket ID for a given user ID
@@ -19,14 +20,16 @@ export class SocketAuthService {
     }
 
     // Remove user from the map when they disconnect
-    public removeUser(socket: socketIO.Socket) {
+    public removeUser(socket: socketIO.Socket, userId?:string) {
         for (let [userId, socketId] of this.clientConnectionSocketIdMap) {
             if (socketId === socket.id) {
                 this.clientConnectionSocketIdMap.delete(userId)
                 console.log(`User ${userId} removed from the map`)
+                
                 break
             }
         }
+        console.log(`map at logout:`, this.clientConnectionSocketIdMap)
     }
 
     // This method will register the events to the socket.
@@ -34,7 +37,23 @@ export class SocketAuthService {
         socket.on('clientAuthenticated', (userId: string) => {
             this.authenticateUser(socket, userId)
         })
-
+        /**
+         * The reason why both of these listeners
+         * (clientLoggedOut and disconnect) are calling
+         * for the same method is because `clientLoggedOut`
+         * is the actual event of client pressing the logout 
+         * button,where as `disconnect` is a built in socket
+         * disconnect function in the socketServer library, 
+         * which is called when user closes the browser tab,
+         * or his internet connection is interrupted suddenly. 
+         * In both of these cases we can to call removeUser() 
+         * method to remove the user from the map of online 
+         * users.
+         */
+        socket.on('clientLoggedOut', () => {
+            this.removeUser(socket)
+        })
+        
         socket.on('disconnect', () => {
             this.removeUser(socket)
         })
